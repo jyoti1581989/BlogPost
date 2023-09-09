9ae20f858ef497b83266ae8e498acd9e45718130
Jyoti Vaishnav <jyoti.vaishnav666@gmail.com>
2023-09-08 23:34:14 -0400

    install pasport,google-oauth,express-session,update partial,insureloggedin

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const passport = require('passport')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  // Which passport strategy is being used?
+  'google',
+  {
+    // Requesting the user's profile and email
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    // prompt: "select_account"
+  }
+))
+
+module.exports = router
--- server.js
+++ server.js
@@ -4,10 +4,13 @@ var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 var methodOverride = require('method-override')
+var session = require('express-session')
+var passport = require('passport')
 
 require('dotenv').config()
 // connect to the database AFTER the config vars are processed
 require('./config/database')
+require('./config/passport')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
@@ -24,6 +27,21 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Add this middleware BELOW passport middleware
+app.use(function (req, res, next) {
+  res.locals.user = req.user
+  next()
+})
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
