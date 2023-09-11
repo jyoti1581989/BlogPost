d996a32c5d2c7ee2c2f4b291c38081ae8ae7aaca
Jyoti Vaishnav <jyoti.vaishnav666@gmail.com>
2023-09-11 16:39:20 -0400

    create one blog and list on the AllBlogs page and commit models/user.js

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,31 @@
+const Blog = require('../models/blog')
+
 module.exports = {
     index,
+    new: newBlog,
+    create
 }
 
 async function index(req, res) {
-    console.log("indielxldx")
-    res.render('blogs/index')
+    const blogs = await Blog.find({})
+    res.render('blogs/index', { title: "All Blogs", blogs })
+}
+
+function newBlog(req, res) {
+    // We'll want to be able to render an  
+    // errorMsg if the create action fails
+    res.render('blogs/new', { title: 'Add Blog', errorMsg: '' })
+}
+
+async function create(req, res) {
+    req.body.userId = req.user._id
+    try {
+        const blog = await Blog.create(req.body)
+        // Redirect to the new movie's show functionality
+        res.redirect(`/blogs/${blog._id}`)
+    } catch (err) {
+        // Typically some sort of validation error
+        console.log(err)
+        res.render('blogs/new', { errorMsg: err.message })
+    }
 }
--- routes/blogs.js
+++ routes/blogs.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 const blogsCtrl = require('../controllers/blogs')
+const ensureLoggedIn = require('../config/ensureLoggedIn')
 
 
-// GET /movies
-
+// GET /blogs
 router.get('/', blogsCtrl.index)
+//GET /blogs/new
+router.get('/new', ensureLoggedIn, blogsCtrl.new)
+//POST /blogs
+router.post('/', ensureLoggedIn, blogsCtrl.create)
 
 module.exports = router
